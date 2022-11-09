033a42c997b9d3c8ec6868beb9fd9403c52aadda
sofiyabalchinova <610185@mail.ru>
2022-11-09 18:40:06 +0300

    Initial Commit

---
+++ lab__2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab__2
+//
+//  Created by София Бальчинова on 09.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
